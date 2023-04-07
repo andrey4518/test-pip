546b8af7bf6aa5af3ce18906e480bdde24c2f38c
Andrey Sinyaev <andrey4518@gmail.com>
2023-04-07 10:16:35 +0300

    setup package pathes

--- setup.py
+++ setup.py
@@ -2,15 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name='test_pip',
-    version='0.0.4',
+    version='0.0.5',
     install_requires=[
         "protobuf==4.22.1"
     ],
-    packages=find_packages(
-        # All keyword arguments below are optional:
-        # where='proto',  # '.' by default
-        include=['proto*'],  # ['*'] by default
-        # exclude=['mypackage.tests'],  # empty by default
-    ),
+    # packages=find_packages(
+    #     # All keyword arguments below are optional:
+    #     # where='proto',  # '.' by default
+    #     include=['proto*'],  # ['*'] by default
+    #     # exclude=['mypackage.tests'],  # empty by default
+    # ),
+    packages=['test_pip.proto'],
+    package_dir = {
+        "test_pip.proto": "proto",
+    },
+    package_data={"test_pip.proto": ["*.proto"]},
     include_package_data=True
 )
